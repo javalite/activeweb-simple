aaf4c91d315ad1c5dd660bb8a504be9488f58faf
Igor Polevoy <igor@expresspigeon.com>
2017-04-17 16:32:49 -0500

    fixed a test

--- src/test/java/app/controllers/MebertControllerSpec.java
+++ src/test/java/app/controllers/MebertControllerSpec.java
@@ -4,7 +4,10 @@ import org.javalite.activeweb.ControllerSpec;
 import org.javalite.common.JsonHelper;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.Map;
+
+import static org.javalite.common.Collections.list;
 
 /**
  * @author igor on 4/17/17.
@@ -13,12 +16,11 @@ public class MebertControllerSpec extends ControllerSpec {
 
     @Test
     public void shouldDisplayChildren(){
-
-        request().params("name", "Igor,Polevoy", "name", "Mike,Ebert").get("index");
-
+        request().params("name",list("Mike,Ebert", "Igor,Polevoy" )).get("index");
         String res = responseContent();
-
-        Map person = (Map) JsonHelper.toList(res).get(0);
-        a(person.get("first_name")).shouldBeEqual("Mike");
+        List<Map> people = JsonHelper.toList(res);
+        the(people.size()).shouldBeEqual(2);
+        a(people.get(0).get("first_name")).shouldBeEqual("Mike");
+        a(people.get(1).get("first_name")).shouldBeEqual("Igor");
     }
 }
