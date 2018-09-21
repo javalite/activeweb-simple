e5c7d8c3868cfe49ba8a8be75b516c04c2c650b9
ipolevoy <igor@expresspigeon.com>
2018-09-21 17:43:09 -0500

    working on issue 399

--- src/main/java/app/controllers/sub/MessagesController.java
+++ src/main/java/app/controllers/sub/MessagesController.java
@@ -2,6 +2,9 @@ package app.controllers.sub;
 
 import org.javalite.activeweb.AppController;
 import org.javalite.activeweb.annotations.POST;
+import org.javalite.common.Util;
+
+import java.io.IOException;
 
 /**
  * @author igor on 9/21/18.
@@ -9,8 +12,8 @@ import org.javalite.activeweb.annotations.POST;
 public class MessagesController extends AppController {
 
     @POST
-    public void index() {
-        respond("ok");
+    public void index() throws IOException {
+        respond("ok: " + Util.read(getRequestInputStream()) );
     }
     public void multiple() {}
     public void single() {}
